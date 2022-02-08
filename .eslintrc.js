0017d2044dd7c0bb8d39dcc1040f955f7dc3437e
es5es5 <es5e5s@nate.com>
2022-02-08 21:19:33 +0900

    Vite + TypeScript -> CLI + JavaScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 }
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { createStore } from 'vuex'
-import router from './router'
-import store from './store'
-import App from './App.vue'
-
-createApp(App).use(router).use(store).mount('#app')
