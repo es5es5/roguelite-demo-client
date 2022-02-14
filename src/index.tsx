ffb0f4eda707e7ed711c488d1472f1c1cece904c
es5es5 <es5e5s@nate.com>
2022-02-14 19:27:11 +0900

    introducing-jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+function formatName(user: any) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
