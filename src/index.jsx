a49c6a8e30cf9a484d2ca7f716a3959bcdd088bd
es5es5 <es5e5s@nate.com>
2022-02-21 18:12:16 +0900

    tsx -> jsx

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,23 @@ function App() {
   );
 }
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App></App>,
+  <Clock />,
   document.getElementById('root')
 );
